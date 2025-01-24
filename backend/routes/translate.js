1a8601d4a1cdf3ed3dfc889b9f859b64f56a90e1
Yannick CHAVANIS <yannick.chavanis.pro@gmail.com>
2025-01-24 10:48:13 +0100

    Translation module added

--- backend/routes/translate.js
+++ backend/routes/translate.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const translate = require('google-translate-api-x');
 
-router.get('/', (req, res) => {
-    res.json({ original: "Hello", translated: "Bonjour", language: 'fr' });
+router.get('/', async (req, res) => {
+    const { message, language } = req.query;
+
+    if (!message || !language) {
+        return res.status(400).json({ error: "Missing message or language in query parameters" });
+    }
+
+    const translation = await translate(message, {to: language});
+
+    res.json({
+        original_message: message,
+        original_language: translation.from.language.iso,
+        translated: translation.text,
+        language: language,
+    });
+});
+
+router.get('/languages', (req, res) => {
+    res.json({
+        languages: ['fr', 'en', 'de', 'es', 'it', 'ja'],
+    });
 });
 
 module.exports = router;
